576ec549e2c6d9d647b1a2dacc23075550a64dfa
kinthomasgeonzon <kinthomas.geonzon@sun-asterisk.com>
2025-03-11 01:28:06 +0800

    Add JWT authentication strategy and guards for task management

---
+++ backend/src/modules/guards/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from '../../prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private readonly prisma: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET as string,
+    });
+  }
+
+  async validate(payload: { id: number }): Promise<User> {
+    const user = await this.prisma.user.findUnique({
+      where: { id: payload.id },
+    });
+    if (!user) {
+      throw new Error('User not found');
+    }
+    return user;
+  }
+}

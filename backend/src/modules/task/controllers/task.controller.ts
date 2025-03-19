38e997466b160e6e1458be6cf3575a5deeffaa75
Kin Geonzon <kinthomas.geonzon@sun-asterisk.com>
2025-03-19 16:15:29 +0800

    Edit Task Implementation (#13)
    * Implement task retrieval and enhance authorization checks
    
    * Add edit task functionality with validation and response DTO
    
    * update
    
    * Remove unused task creation and retrieval methods from TaskController and TaskService

--- backend/src/modules/task/controllers/task.controller.ts
+++ backend/src/modules/task/controllers/task.controller.ts
@@ -1,7 +1,18 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { Request } from 'express';
 import { TaskAuthGuard } from '../../guards/task-auth.guard';
 import { CreateTaskDto } from '../dto/req.create-task.dto';
+import { EditTaskDto } from '../dto/req.edittasks.dto';
 import { TaskService } from '../services/task.service';
 
 interface AuthenticatedRequest extends Request {
@@ -13,6 +24,13 @@ interface AuthenticatedRequest extends Request {
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
+  @Patch(':id')
+  async editTask(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: EditTaskDto,
+  ) {
+    return await this.taskService.editTask(id, dto);
+  }
   @Post()
   async create(@Req() req: AuthenticatedRequest, @Body() dto: CreateTaskDto) {
     const user = req.user;

13544939608bb4759f68811ea15101a5786690f7
Kin Geonzon <kinthomas.geonzon@sun-asterisk.com>
2025-03-26 16:18:39 +0800

    Add environment configuration and update CORS settings; refactor Kanban component props and remove unused styles (#20)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,18 +1,27 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const allowedOrigins = [
+    process.env.LOCAL_BE_URL || 'http://localhost:3000',
+    process.env.SERVER_BE_URL || 'https://taskman-krpexpd5u-kins-projects-a2930a32.vercel.app',
+  ];
+
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: allowedOrigins,
     credentials: true,
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'], 
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   });
 
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 4000);
+  const PORT = process.env.PORT || 4000;
+  await app.listen(PORT);
 }
 
 bootstrap().catch((err) => console.error(err));
